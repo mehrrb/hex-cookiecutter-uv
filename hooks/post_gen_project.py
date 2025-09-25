e160c62b8497f31474672d3698079538850331a2
mohammadjavad <mj.eivazi95@gmail.com>
2025-09-26 02:47:36 +0330

    Initial hexagonal Django REST + uv Cookiecutter template

--- hooks/post_gen_project.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import subprocess
-import os
-
-project_dir = os.path.realpath(os.path.curdir)
-subprocess.run(["git", "init"], cwd=project_dir)
-subprocess.run(["git", "add", "."], cwd=project_dir)
-subprocess.run(["git", "commit", "-m", "Initial commit"], cwd=project_dir)
